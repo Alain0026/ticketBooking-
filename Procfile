3a50aec5c6bb446ffb88d19e845b8161e34893da
Alain0026 <152007509+Alain0026@users.noreply.github.com>
2025-06-21 12:58:28 +0100

    database

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: bash -c "python manage.py migrate && gunicorn ticketbooking.wsgi:application --bind 0.0.0.0:$PORT"
+web: python manage.py collectstatic --noinput
--- ticketbooking/settings.py
+++ ticketbooking/settings.py
@@ -97,8 +97,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / 'static']
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Media files
 MEDIA_URL = '/media/'
