fe0e762a39cff4dc5adffc0c0bafcc39e655ae99
Alain0026 <152007509+Alain0026@users.noreply.github.com>
2025-06-22 01:33:37 +0100

    aggiornamenti

--- ticketbooking/settings.py
+++ ticketbooking/settings.py
@@ -134,7 +134,7 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # WhiteNoise pour compresser les fichiers statiques (prod)
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
